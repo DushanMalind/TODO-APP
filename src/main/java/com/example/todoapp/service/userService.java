30ff46d59129269d9ebf1ef55db332f8f944024b
DushanMalind <dushanmalinda010@gmail.com>
2024-10-30 18:47:48 +0530

    userService interface

---
+++ src/main/java/com/example/todoapp/service/userService.java
@@ -0,0 +1,15 @@
+package com.example.todoapp.service;
+
+import com.example.todoapp.dto.UserDTO;
+
+/**
+ * `@authority` DUSHAN MALINDA
+ * 18:42
+ * 30/10/2024
+ * ASUS
+ * Todo-App
+ * com.example.todoapp.service
+ */
+public interface userService {
+    int saveUser(UserDTO userDTO);
+}
