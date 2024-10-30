3248ff752c5590d8e23404eb6c42a7fe40b191ee
DushanMalind <dushanmalinda010@gmail.com>
2024-10-30 18:23:41 +0530

    UserDTO done

---
+++ src/main/java/com/example/todoapp/dto/UserDTO.java
@@ -0,0 +1,17 @@
+package com.example.todoapp.dto;
+
+import java.util.UUID;
+
+/**
+ * `@authority` DUSHAN MALINDA
+ * 18:21
+ * 30/10/2024
+ * ASUS
+ * Todo-App
+ * com.example.todoapp.dto
+ */
+public class UserDTO {
+    private UUID id;
+    private String email;
+    private String password;
+}
