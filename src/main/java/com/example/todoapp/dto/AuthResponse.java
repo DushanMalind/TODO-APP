d998e4472134516954e3f626a63d99e0f76f2195
DushanMalind <dushanmalinda010@gmail.com>
2024-10-30 18:30:18 +0530

    AuthResponse

---
+++ src/main/java/com/example/todoapp/dto/AuthResponse.java
@@ -0,0 +1,17 @@
+package com.example.todoapp.dto;
+
+import lombok.Data;
+
+/**
+ * `@authority` DUSHAN MALINDA
+ * 18:29
+ * 30/10/2024
+ * ASUS
+ * Todo-App
+ * com.example.todoapp.dto
+ */
+@Data
+public class AuthResponse {
+    private String token;
+    private String message;
+}
