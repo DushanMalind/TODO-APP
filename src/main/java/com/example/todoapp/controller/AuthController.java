b4023e50b82c6cf2dc2c8ea041575698f533b06a
DushanMalind <dushanmalinda010@gmail.com>
2024-10-31 12:52:02 +0530

    AuthController Create

---
+++ src/main/java/com/example/todoapp/controller/AuthController.java
@@ -0,0 +1,22 @@
+package com.example.todoapp.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * `@authority` DUSHAN MALINDA
+ * 12:49
+ * 31/10/2024
+ * ASUS
+ * Todo-App
+ * com.example.todoapp.controller
+ */
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+@CrossOrigin
+public class AuthController {
+
+}
